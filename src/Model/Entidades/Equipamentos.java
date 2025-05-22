98c6f9aeb16bdef09320cc667a84afae5f84b7a5
Clemerson <clemesonsilva736@gmail.com>
2025-05-22 13:49:06 -0300

    Implementação da classe Equipamentos de acordo com o UML do projeto

--- src/Model/Entidades/Equipamentos.java
+++ src/Model/Entidades/Equipamentos.java
@@ -14,7 +14,7 @@ public class Equipamentos {
     }
 
     public void setNome(String nome) {
-        if (!nome.isBlank()) {
+        if (validarEqui(nome)) {
             this.nome = nome;
         } else {
             System.out.println("Erro! Não pode estar em branco.");
@@ -26,7 +26,7 @@ public class Equipamentos {
     }
 
     public void setNumeroSerie(int numeroSerie) {
-        if (numeroSerie > 0) {
+        if (validarEquiSerie(numeroSerie)) {
             this.numeroSerie = numeroSerie;
         } else {
             System.out.println("Erro! Não pode ser menor que zero.");
@@ -38,7 +38,7 @@ public class Equipamentos {
     }
 
     public void setPreco(double preco) {
-        if (preco > 0) {
+        if (validarEqui(preco)) {
             this.preco = preco;
         } else {
             System.out.println("Erro! Não pode ser menor que zero.");
@@ -50,7 +50,7 @@ public class Equipamentos {
     }
 
     public void setQuantidade(int quantidade) {
-        if (quantidade >= 0) {
+        if (validarEqui(quantidade)) {
             this.quantidade = quantidade;
         } else {
             System.out.println("Erro! Não pode ser menor que zero.");
@@ -62,7 +62,7 @@ public class Equipamentos {
     }
 
     public void setLocal(Locais local) {
-        if (local != null) {
+        if (validarEqui(local)) {
             this.local = local;
         } else {
             System.out.println("Erro! Não pode estar em branco.");
@@ -74,7 +74,7 @@ public class Equipamentos {
     }
 
     public void setResponsavel(Responsavel responsavel) {
-        if (responsavel != null) {
+        if (validarEqui(responsavel)) {
             this.responsavel = responsavel;
         } else {
             System.out.println("Erro! Não pode estar em branco.");
@@ -99,5 +99,192 @@ public class Equipamentos {
         setQuantidade(quantidade);
         setLocal(local);
         setResponsavel(responsavel);
+    }
+
+    // métodos para vender e comprar equipamentos
+    public void comprar(int numeroSerie, int quantidade) {}
+
+    // métodos para pesquisar equipamentos
+    public void pesquisar(Responsavel responsavel) {
+        if (validarEqui(responsavel)) System.out.println(
+            "Pesquisa pelo equipamento feita com sucesso!"
+        );
+        else System.out.println(
+            "Dado para pesquisa do equipamento não pode ser deixado em branco!"
+        );
+    }
+
+    public void pesquisar(double preco) { // pode ser que, no futuro, seja melhor mudar esse metodo para pesquisar em um faixa de valores
+        if (validarEqui(preco)) System.out.println(
+            "Pesquisa pelo equipamento feita com sucesso!"
+        );
+        else System.out.println(
+            "Dado para pesquisa do equipamento não pode ser deixado em branco ou menor que zero!"
+        );
+    }
+
+    public void pesquisar(String nome) {
+        if (validarEqui(nome)) System.out.println(
+            "Pesquisa pelo equipamento feita com sucesso!"
+        );
+        else System.out.println(
+            "Dado para pesquisa do equipamento não pode ser deixado em branco!"
+        );
+    }
+
+    public void pesquisar(int numSerie) {
+        if (numSerie > 0) System.out.println(
+            "Pesquisa pelo equipamento feita com sucesso!"
+        );
+        else System.out.println(
+            "Dado para pesquisa do equipamento não pode ser deixado em branco ou menor que zero!"
+        );
+    }
+
+    // métodos para editar Equipamentos
+    public void editar(int numeroSerie) {
+        if (validarEquiSerie(numeroSerie)) System.out.println(
+            "Numero de série do equipamento alterado com sucesso!"
+        );
+        else System.out.println(
+            "Numero de série do equipamento não pode ser deixado em branco ou menor que zero!"
+        );
+    }
+
+    public void editar(Responsavel responsavel) {
+        if (validarEqui(responsavel)) System.out.println(
+            "Responsavel pelo equipamento alterado com sucesso!"
+        );
+        else System.out.println(
+            "Responsavel pelo equipamento não pode ser deixado em branco ou menor que zero!"
+        );
+    }
+
+    public void editar(Locais local) {
+        if (validarEqui(local)) System.out.println(
+            "Local do equipamento alterado com sucesso!"
+        );
+        else System.out.println(
+            "Local do equipamento não pode ser deixado em branco ou menor que zero!"
+        );
+    }
+
+    public void editarQtd(int quantidade) {
+        if (validarEqui(quantidade)) System.out.println(
+            "Quantidade do equipamento alterado com sucesso!"
+        );
+        else System.out.println(
+            "Quantidade do equipamento não pode ser deixado em branco ou menor que zero!"
+        );
+    }
+
+    public void editar(double preco) {
+        if (validarEqui(preco)) System.out.println(
+            "Preço do equipamento alterado com sucesso!"
+        );
+        else System.out.println(
+            "Preço do equipamento não pode ser deixado em branco ou menor que zero!"
+        );
+    }
+
+    public void editar(String nome) {
+        if (validarEqui(nome)) System.out.println(
+            "Nome do equipamento alterado com sucesso!"
+        );
+        else System.out.println(
+            "o nome da casa não pode ser deixado em branco!"
+        );
+    }
+
+    public void editar(
+        String nome,
+        int numeroSerie,
+        double preco,
+        int quantidade,
+        Locais local,
+        Responsavel responsavel
+    ) {
+        if (
+            validarEqui(
+                nome,
+                numeroSerie,
+                preco,
+                quantidade,
+                local,
+                responsavel
+            )
+        ) System.out.println("Equipamento editado com sucesso!");
+        else System.out.println("Faltam dados para a edição!");
+    }
+
+    public void cadastrar(
+        String nome,
+        int numeroSerie,
+        double preco,
+        int quantidade,
+        Locais local,
+        Responsavel responsavel
+    ) {
+        if (
+            validarEqui(
+                nome,
+                numeroSerie,
+                preco,
+                quantidade,
+                local,
+                responsavel
+            )
+        ) System.out.println("Equipamento criado com sucesso!");
+        else System.out.println("Faltam dados para o cadastro!");
+    }
+
+    // os metodos de validação foram feitos pois estavam sendo chamados muitas vezes, por isso o reaproveitamento.
+    public static boolean validarEqui(double preco) {
+        if (preco >= 0) return true;
+        else return false;
+    }
+
+    public static boolean validarEqui(String nome) {
+        if (!nome.isBlank()) return true;
+        else return false;
+    }
+
+    public static boolean validarEquiSerie(int numeroSerie) {
+        if (numeroSerie > 0) return true;
+        else return false;
+    }
+
+    public static boolean validarEqui(int quantidade) {
+        if (quantidade >= 0) return true;
+        else return false;
+    }
+
+    public static boolean validarEqui(Locais local) {
+        if (local != null) return true;
+        else return false;
+    }
+
+    public static boolean validarEqui(Responsavel responsavel) {
+        if (responsavel != null) return true;
+        else return false;
+    }
+
+    public static boolean validarEqui(
+        String nome,
+        int numeroSerie,
+        double preco,
+        int quantidade,
+        Locais local,
+        Responsavel responsavel
+    ) {
+        if (
+            validarEqui(nome) &&
+            validarEquiSerie(numeroSerie) &&
+            validarEqui(preco) &&
+            validarEqui(quantidade) &&
+            validarEqui(local) &&
+            validarEqui(responsavel)
+        ) return true;
+        else return false;
     }
 }
