7b2c52d47d02aab908d96852f64956428816516b
Clemerson <clemesonsilva736@gmail.com>
2025-05-22 12:29:27 -0300

    Implementação da classe locais de acordo com o UML do projeto

--- src/Model/Entidades/Locais.java
+++ src/Model/Entidades/Locais.java
@@ -37,4 +37,25 @@ public class Locais {
         setNomeCompartimento(nomeCompartimento);
         setNomeCasa(nomeCasa);
     }
+    public void editarCompartimento(String nomeCompartimento){
+        if(!nomeCompartimento.isBlank()) System.out.println("Cadastro feito com sucesso");
+        else  System.out.println("O nome do compartimento da casa não pode ser deixado em branco");
+    }
+    public void editarCasa(String nomeCasa){
+        if(!nomeCasa.isBlank()) System.out.println("Cadastro feito com sucesso");
+        else  System.out.println("O nome casa não pode ser deixado em branco");
+    }
+    public void editar(String nomeCompartimento, String nomeCasa){
+        if(validarLocais(nomeCompartimento, nomeCasa)) System.out.println("Cadastro feito com sucesso");
+        else  System.out.println("Nenhum dos valores pode ser deixado em branco");
+    }
+    public void cadastrar(String nomeCompartimento, String nomeCasa){
+        if(validarLocais(nomeCompartimento, nomeCasa)) System.out.println("Cadastro feito com sucesso");
+        else  System.out.println("Nenhum dos valores pode ser deixado em branco");
+    }
+
+    public static boolean validarLocais(String nomeCompartimento, String nomeCasa){
+        if(!nomeCasa.isBlank() && !nomeCompartimento.isBlank()) return true;
+        else return false;
+    }
 }
