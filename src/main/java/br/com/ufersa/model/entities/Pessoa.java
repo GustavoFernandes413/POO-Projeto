d7bf3cfd33e703a770e5e893fe189c313a2abcb6
Clemerson <clemesonsilva736@gmail.com>
2025-06-13 11:00:23 -0300

    criacao da classe Pessoa. Ela será usada como superclasse das classes Cliente e Responsavel

---
+++ src/main/java/br/com/ufersa/model/entities/Pessoa.java
@@ -0,0 +1,61 @@
+package java.br.com.ufersa.model.entities;
+
+public class Pessoa {
+
+    private String nome;
+    private String endereco;
+
+    public Pessoa(String nome, String endereco) {
+        setNome(nome);
+        setEndereco(endereco);
+    }
+
+    public void editarEnd(String endereco) {
+        if (validarAttrString(endereco)) {
+            System.out.println("Edição feita com sucesso");
+        } else {
+            System.out.println(
+                "O atributo a ser editado não pode ser deixado em branco"
+            );
+        }
+    }
+
+    public void editarNome(String nome) {
+        if (validarAttrString(nome)) {
+            System.out.println("Edição feita com sucesso");
+        } else {
+            System.out.println(
+                "O atributo a ser editado não pode ser deixado em branco"
+            );
+        }
+    }
+
+    public String getEndereco() {
+        return endereco;
+    }
+
+    public void setEndereco(String endereco) {
+        if (validarAttrString(endereco)) {
+            this.endereco = endereco;
+        } else {
+            System.out.println("Erro! Não pode estar em branco.");
+        }
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        if (validarAttrString(nome)) {
+            this.nome = nome;
+        } else {
+            System.out.println("Erro! Não pode estar em branco.");
+        }
+    }
+
+    public static boolean validarAttrString(String str) {
+        if (!str.isBlank()) return true;
+        return false;
+    }
+}
