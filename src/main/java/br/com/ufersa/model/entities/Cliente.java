a2f9036b9232dc05ecd613b88b3bc2af0d2ee2e0
Clemerson <clemesonsilva736@gmail.com>
2025-07-19 18:15:44 -0300

    fix: 'limpeza' da entidade Cliente, retirando os metodos que foram movidos à camada de servicos

--- src/main/java/br/com/ufersa/model/entities/Cliente.java
+++ src/main/java/br/com/ufersa/model/entities/Cliente.java
@@ -9,7 +9,6 @@ import jakarta.persistence.*;
 public class Cliente extends Pessoa {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long Id;
     @Column(nullable = false)
     private String cpf;
 
@@ -38,32 +37,6 @@ public class Cliente extends Pessoa {
         }
     }
 
-    public void editarCpf(String cpf) {
-        if (validarAttrString(cpf)) {
-            System.out.println("Edição feita com sucesso");
-        } else {
-            System.out.println(
-                "O atributo a ser editado não pode ser deixado em branco"
-            );
-        }
-    }
-
-    public void editar(String nome, String endereco, String cpf) {
-        if (validarEntradaCliente(nome, endereco, cpf)) {
-            System.out.println("Editção feita com sucesso");
-        } else {
-            System.out.println("Nenhum dos valores pode ser deixado em branco");
-        }
-    }
-
-    public void cadastrar(String nome, String endereco, String cpf) {
-        if (validarEntradaCliente(nome, endereco, cpf)) {
-            System.out.println("Cadastro feito com sucesso");
-        } else {
-            System.out.println("Nenhum dos valores pode ser deixado em branco");
-        }
-    }
-
     public Long getId() {
         return Id;
     }
@@ -72,12 +45,4 @@ public class Cliente extends Pessoa {
         Id = id;
     }
 
-    // como a validação será usada mais de uma vez, decidi reaproveitá-la
-    public static boolean validarEntradaCliente(
-        String nome,
-        String endereco,
-        String cpf
-    ) {
-        return (!nome.isBlank() && !endereco.isBlank() && cpf.isBlank());
-    }
-}
+ }
