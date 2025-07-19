1fa46546773894e82af2af9695ab4b7839ba4a84
Clemerson <clemesonsilva736@gmail.com>
2025-07-19 18:16:10 -0300

    fix: 'limpeza' da entidade Cliente, retirando os metodos que foram movidos à camada de servicos

--- src/main/java/br/com/ufersa/model/entities/Cliente.java
+++ src/main/java/br/com/ufersa/model/entities/Cliente.java
@@ -37,12 +37,6 @@ public class Cliente extends Pessoa {
         }
     }
 
-    public Long getId() {
-        return Id;
-    }
-
-    public void setId(Long id) {
-        Id = id;
-    }
+    
 
  }
