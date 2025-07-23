64c33722fc083fcc5010190806e7c0a452cdce71
Clemerson <clemesonsilva736@gmail.com>
2025-07-23 15:47:19 -0300

    configuracao do JavaFx no projeto

---
+++ src/main/java/module-info.java
@@ -0,0 +1,21 @@
+module br.com.ufersa {
+    requires javafx.controls;
+    requires javafx.web;
+    requires javafx.fxml;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires net.synedra.validatorfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+    requires eu.hansolo.tilesfx;
+    requires com.almasb.fxgl.all;
+    requires jakarta.persistence;
+    requires java.desktop;
+
+    requires org.hibernate.orm.core;
+    opens br.com.ufersa.model.entities to org.hibernate.orm.core;
+    opens br.com.ufersa.presenter to javafx.fxml;
+    exports br.com.ufersa.view;
+    opens br.com.ufersa.view to javafx.fxml;
+}
